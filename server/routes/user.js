08c5faf070aaaec1b2c54024a08a9633e6105c1e
Or Ron <or.ron7@gmail.com>
2021-04-04 12:44:22 +0300

    remove mongoos from user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
 const services = require("./services");
 
